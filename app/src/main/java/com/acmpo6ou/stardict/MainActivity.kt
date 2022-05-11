ec5fb0fae8e02e1da8ab3bddd21716ae84d0ea12
acmpo6ou <acmpo6ou@gmail.com>
2022-05-11 20:41:52 +0200

    ✨ Implement app bar title

--- app/src/main/java/com/acmpo6ou/stardict/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/stardict/MainActivity.kt
@@ -3,12 +3,9 @@ package com.acmpo6ou.stardict
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.acmpo6ou.stardict.ui.theme.StarDictTheme
 
@@ -17,27 +14,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StarDictTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                AppBar()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppBar() {
+    SmallTopAppBar(
+        title = { Text("✨StarDict✨") }
+    )
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    StarDictTheme {
-        Greeting("Android")
-    }
+fun AppBarPreview() {
+    AppBar()
 }
