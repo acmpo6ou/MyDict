6daa236c292323848c168e5a62f6b8866a759d23
acmpo6ou <acmpo6ou@gmail.com>
2022-05-12 11:18:43 +0200

    🎨 Fix color palette

--- app/src/main/java/com/acmpo6ou/stardict/ui/theme/Color.kt
+++ app/src/main/java/com/acmpo6ou/stardict/ui/theme/Color.kt
@@ -2,6 +2,6 @@ package com.acmpo6ou.stardict.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Coral = Color(0xFF5F5DFF)
-val SkyBlue = Color(0x00CCBFFF)
-val DarkBlue = Color(0x3F7C85FF)
+val Coral = Color(0xFFFF5F5D)
+val SkyBlue = Color(0xFF00CCBF)
+val DarkBlue = Color(0xFF3F7C85)
