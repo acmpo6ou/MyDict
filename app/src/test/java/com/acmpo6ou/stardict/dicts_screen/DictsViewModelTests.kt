357637de925c3d4d7fd7369b012986fc0ccc71b4
acmpo6ou <acmpo6ou@gmail.com>
2022-05-16 20:02:24 +0200

    ⚙️  Implement setupInputResolver

--- app/src/test/java/com/acmpo6ou/stardict/dicts_screen/DictsViewModelTests.kt
+++ app/src/test/java/com/acmpo6ou/stardict/dicts_screen/DictsViewModelTests.kt
@@ -19,19 +19,30 @@
 
 package com.acmpo6ou.stardict.dicts_screen
 
+import android.content.ContentResolver
+import android.net.Uri
+import android.os.ParcelFileDescriptor
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 import java.io.File
+import java.io.FileInputStream
 
+@RunWith(RobolectricTestRunner::class)
 class DictsViewModelTests {
     @get:Rule
     val taskExecutorRule = InstantTaskExecutorRule()
+
     lateinit var model: DictsViewModel
     private val srcDir = "/tmp/StarDict"
+
+    private val contentResolver: ContentResolver = mock()
 
     @Before
     fun setupSrcDir() {
@@ -40,10 +51,22 @@ class DictsViewModelTests {
         dir.mkdir()
     }
 
+    private fun setupInputResolver(location: String, uri: Uri) {
+        val descriptor: ParcelFileDescriptor = mock()
+        val fis = FileInputStream(File(location))
+
+        whenever(descriptor.fileDescriptor).thenReturn(fis.fd)
+        whenever(contentResolver.openFileDescriptor(uri, "r"))
+            .thenReturn(descriptor)
+    }
+
     @Before
     fun setup() {
         model = DictsViewModel()
-        model.app = mock { on { SRC_DIR } doReturn srcDir }
+        model.app = mock {
+            on { SRC_DIR } doReturn srcDir
+            on { contentResolver } doReturn contentResolver
+        }
     }
 
     @Test
