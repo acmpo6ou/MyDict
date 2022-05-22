311ad0a8c834bfc881080c8a54bf146432d689ea
acmpo6ou <acmpo6ou@gmail.com>
2022-05-22 17:50:26 +0200

    🔍 Style search field

--- app/src/main/java/com/acmpo6ou/stardict/MainActivity.kt
+++ app/src/main/java/com/acmpo6ou/stardict/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
@@ -40,6 +41,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -184,6 +186,8 @@ fun SearchField(model: MainViewModel) {
             .padding(horizontal = 8.dp)
             .fillMaxWidth(),
         value = searchText,
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+        maxLines = 1,
         onValueChange = { model.search(it) },
         label = { Text("Search") },
         leadingIcon = {
