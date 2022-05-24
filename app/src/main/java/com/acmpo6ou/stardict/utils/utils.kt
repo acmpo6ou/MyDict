02ab7358486831b48b44def1b4c0f075f89fd6d2
acmpo6ou <acmpo6ou@gmail.com>
2022-05-24 12:50:54 +0200

    ✨ Make dialog text white

---
+++ app/src/main/java/com/acmpo6ou/stardict/utils/utils.kt
@@ -0,0 +1,66 @@
+/*
+ * StarDict implementation for Android.
+ * Copyright (C) 2022  Bohdan Kolvakh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.acmpo6ou.stardict.utils
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.vanpra.composematerialdialogs.MaterialDialogScope
+
+@Composable
+fun MaterialDialogScope.iconTitle(
+    titleText: String,
+    icon: @Composable () -> Unit = {},
+) {
+    Row(
+        modifier = Modifier
+            .padding(start = 24.dp, end = 24.dp)
+            .height(64.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        icon()
+        Spacer(Modifier.width(14.dp))
+        Text(
+            text = titleText,
+            color = Color.White,
+            style = MaterialTheme.typography.headlineMedium,
+        )
+    }
+}
+
+@Composable
+fun MaterialDialogScope.message(text: String? = null, @StringRes res: Int? = null) {
+    val messageText = text ?: stringResource(res!!)
+
+    Text(
+        text = messageText,
+        color = Color.White,
+        style = MaterialTheme.typography.bodyLarge,
+        modifier = Modifier
+            .padding(bottom = 28.dp, start = 24.dp, end = 24.dp)
+    )
+}
